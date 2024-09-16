d60eb8b81ad5c05adad0ba72be3021791b2978ea
MajkiX <Mateuszzawadzki01@interia.pl>
2024-09-16 13:47:42 +0200

    StoreProvider

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import '../App.css'
+import StoreProvider from '../store/StoreProvider';
+
+function App() {
+  return (
+    <StoreProvider>
+      <div className="App">
+      
+      </div>
+    </StoreProvider>
+  );
+}
+
+export default App;
