adc55a6c76bbf0a2d4aae5dff960a0038c75a61d
MajkiX <Mateuszzawadzki01@interia.pl>
2024-09-25 14:10:51 +0200

    Buy button, Courses titles

--- src/components/Course/Course.jsx
+++ src/components/Course/Course.jsx
@@ -1,18 +1,49 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import bemCssModules from 'bem-css-modules'
 import { default as CourseStyles } from './Course.module.scss'
+import request from '../../helpers/request';
+import { StoreContext } from '../../store/StoreProvider';
+import { useNavigate } from 'react-router';
 
 const style = bemCssModules(CourseStyles)
 
-const Course = ({ authors, img, price, title }) => {
+const Course = ({ authors, id, isUserContext, img, price, title }) => {
+
+  const { user, setUser } = useContext(StoreContext)
+  const history = useNavigate()
+
   const allAuthors = authors.join(', ')
+
+  const isUserLogged = Boolean(user)
+
+  const handleOnClick = async () => {
+    try {
+      const { data, status } = await request.patch(
+        '/users',
+        {
+          login: user.login,
+          courseId: id,
+        }
+      )
+      if (status === 202) {
+        setUser(data.user)
+        history('my-courses')
+      }
+    } catch (error) {
+      console.warn(error)
+    }
+  }
+
+  const shouldBeBoyButtonVisible = isUserLogged && !isUserContext
+
   return (
     <li>
       <article className={style()}>
-        <h3 className={style('title')}></h3>
+        <h3 className={style('title')}>{title}</h3>
         <img src={img} alt={title} className={style('image')} />
         <p className={style('price')}>{`Koszt kursu: ${price}zł`}</p>
         <p className={style('authors')}>{`Autorzy kursu: ${allAuthors}`}</p>
+        {shouldBeBoyButtonVisible && <button onClick={handleOnClick}>Zakup ten kurs</button>}
       </article>
     </li>
   );
